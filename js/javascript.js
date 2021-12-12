4b3cec4d0b3538dad0481097724e6c4aa709022a
George Bradley <55600269+GeorgeBradley@users.noreply.github.com>
2021-12-12 15:10:25 +0000

    Added a scrolling effect to scrolling anchor links to jump to a specific part of a web page

--- js/javascript.js
+++ js/javascript.js
@@ -55,3 +55,16 @@ btnBurgerMenu.addEventListener('click', function(){
 
 });
 
+
+// Scroll to section (property-details page)
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+  anchor.addEventListener("click", function(e){
+
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+
+      behavior: "smooth"
+    });
+  });
+})
